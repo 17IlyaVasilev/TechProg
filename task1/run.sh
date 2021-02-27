9a29e682971f3912fc36000264ab721b84e0541c
17IlyaVasilev <73096983+17IlyaVasilev@users.noreply.github.com>
2021-02-27 22:29:07 +0300

    Update run.sh

--- task1/run.sh
+++ task1/run.sh
@@ -1,6 +1,6 @@
 # ./run.sh <./run.sh <путь к директории> <расширение> <название папки для бэкапа> <имя архива с бэкапом>
 
 mkdir $3
-find $1 -name "*.$2" -exec cp  {} $3 \;
+find $1 -name "*.$2" -exec cp --backup=numbered {} $3 \;
 tar $4 $3
 echo done
