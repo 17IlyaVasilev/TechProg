c6ad86dcc1941046118437ade661f507e663a798
17IlyaVasilev <73096983+17IlyaVasilev@users.noreply.github.com>
2021-02-28 03:24:28 +0300

    Delete task1 directory

--- task1/run.sh
+++
@@ -1,6 +0,0 @@
-# ./run.sh <./run.sh <путь к директории> <расширение> <название папки для бэкапа> <имя архива с бэкапом>
-
-mkdir $3
-find $1 -name "*.$2" -exec cp --backup=numbered {} $3 \;
-tar -czf $4 $3
-echo done
